e87aa2d00a22ca9390759ee8d10c671e00decc16
mar-v-in <github@rvin.mooo.com>
2015-10-25 16:19:33 +0100

    Fix path to blacklist file

--- fastd-blacklist.sh
+++ fastd-blacklist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PEER_KEY=$1
 
-if /bin/grep -Fq $PEER_KEY /etc/fastd/fastd-blacklist.json; then
+if /bin/grep -Fq $PEER_KEY /etc/fastd/fastdbl/fastd-blacklist.json; then
 	exit 1
 else
 	exit 0
